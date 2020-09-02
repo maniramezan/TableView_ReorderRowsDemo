0e9905c143c86b6612a3813d281ec669d1aaebfd
Mani Ramezan <mani.ramezan@gmail.com>
2020-09-02 13:10:59 -0400

    Datasource refactoring

--- MultiSelectionDemo/ViewController.swift
+++ MultiSelectionDemo/ViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
     /// Datasource
-    var animal = ["lion","dog","cat","fish"]
-    var birds = ["pigeon","crow","cucco","kingfisher"]
-    var humans = ["Ritzy","Patrick","Jack"]
+    var dataSource = [
+        0: ["lion","dog","cat","fish"],
+        1: ["pigeon","crow","cucco","kingfisher"],
+        2: ["Ritzy","Patrick","Jack"]
+    ]
+
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -27,13 +30,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return animal.count
-        } else if section == 1 {
-            return birds.count
-        } else {
-            return humans.count
-        }
+        return dataSource[section]?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,13 +39,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
                 for: indexPath)
                 as? DemoTableViewCell
         else { return UITableViewCell() }
-        if indexPath.section == 0 {
-            cell.nameLabel.text = animal[indexPath.row]
-        } else if indexPath.section == 1 {
-            cell.nameLabel.text = birds[indexPath.row]
-        } else {
-            cell.nameLabel.text = humans[indexPath.row]
-        }
+        cell.nameLabel.text = dataSource[indexPath.section]?[indexPath.row]
         return cell
     }
     
@@ -80,31 +71,13 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         ///Find the cell which is being moved
-        var product=""
-        if sourceIndexPath.section == 0 {
-            product = animal[sourceIndexPath.row]
-        } else if sourceIndexPath.section == 1 {
-            product = birds[sourceIndexPath.row]
-        } else {
-            product = humans[sourceIndexPath.row]
-        }
+        let product = dataSource[sourceIndexPath.section]?[sourceIndexPath.row] ?? ""
+        
         /// find the section where cell is being added
-        if destinationIndexPath.section == 0 {
-            animal.insert(product, at: destinationIndexPath.row)
-        }else if sourceIndexPath.section == 1 {
-            birds.insert(product, at: destinationIndexPath.row)
-        } else {
-            humans.insert(product, at: destinationIndexPath.row)
-        }
+        dataSource[sourceIndexPath.section]?.insert(product, at: destinationIndexPath.row)
         
         /// remove from source
-        if sourceIndexPath.section == 0 {
-            animal.remove(at: sourceIndexPath.row)
-        } else if sourceIndexPath.section == 1 {
-            birds.remove(at: sourceIndexPath.row)
-        } else {
-            humans.remove(at: sourceIndexPath.row)
-        }
+        dataSource[sourceIndexPath.section]?.remove(at: sourceIndexPath.row)
     }
 }
 
