8f11b2416088abe257132cb3132303c93f6e3ab3
Ritika <rgupt008@hersheys.com>
2020-09-02 19:18:56 +0530

    Initial Commit

---
+++ MultiSelectionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiSelectionDemo
+//
+//  Created by Mac_Admin on 02/09/20.
+//  Copyright © 2020 Mac_Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
